55a2bf1af1095f5485ce27d9472d6356beb04eae
Mads <mads_beier@hotmail.com>
2019-02-25 11:54:29 +0100

    New button

--- src/ultimatettt/gui/controller/StartWindowController.java
+++ src/ultimatettt/gui/controller/StartWindowController.java
@@ -5,9 +5,16 @@
  */
 package ultimatettt.gui.controller;
 
+import com.jfoenix.controls.JFXButton;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -16,6 +23,9 @@ import javafx.fxml.Initializable;
  */
 public class StartWindowController implements Initializable {
 
+    @FXML
+    private JFXButton btnPVP;
+
     /**
      * Initializes the controller class.
      */
@@ -23,5 +33,19 @@ public class StartWindowController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void playPVP(ActionEvent event) throws IOException {
+        Stage st = (Stage) btnPVP.getScene().getWindow();
+        st.close();
+        Stage stage = (Stage) btnPVP.getScene().getWindow(); //new Stage();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/ultimatettt/gui/view/Main.fxml"));
+        stage.setScene(new Scene(loader.load()));
+//        stage.setHeight(stage.getHeight());
+//        stage.setWidth(stage.getWidth());
+//        stage.setMinHeight(600);
+//        stage.setMinWidth(800);
+        stage.show();
+    }
     
 }
